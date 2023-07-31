78dc7bbe5d7c6a50085f12fdc2664c0f1c64a9f1
jeff0102 <jefferson.jvega@gmail.com>
2023-07-31 16:16:50 -0300

    updated version

--- module 2/exercicios-backend-leitura-escrita-arquivos/02/src/controller/pokemons.mjs
+++ module 2/exercicios-backend-leitura-escrita-arquivos/02/src/controller/pokemons.mjs
@@ -4,14 +4,12 @@ const pokemons = async (req, res) => {
   try {
     const { pagina } = req.query;
 
-    // Intenta obtener la lista de Pokémon utilizando la función listarPokemons.
-    // Si ocurre una excepción, será capturada por el bloque catch.
     const listaPokemons = await listarPokemons(pagina ?? 5);
 
     return res.json(listaPokemons);
   } catch (error) {
     // En caso de una excepción, aquí puedes manejar el error y responder con un mensaje de error apropiado.
-    return res.status(500).json({ error: 'Erro ao obter a lista de pokémons' });
+    return res.status(500).json({ error: error.message });
   }
 };
 
@@ -19,16 +17,13 @@ const pokemon = async (req, res) => {
   try {
     const { idOuNome } = req.params;
 
-    // Intenta obtener el detalle del Pokémon utilizando la función detalharPokemon.
-    // Si ocurre una excepción, será capturada por el bloque catch.
     const pokemonEncontrado = await detalharPokemon(idOuNome);
 
     const { id, name, height, weight, base_experience, forms, abilities, species } = pokemonEncontrado;
 
     return res.json({ id, name, height, weight, base_experience, forms, abilities, species });
   } catch (error) {
-    // En caso de una excepción, aquí puedes manejar el error y responder con un mensaje de error apropiado.
-    return res.status(500).json({ error: 'Erro ao obter o detalhe do Pokémon' });
+    return res.status(500).json({ error: error.message });
   }
 };
 

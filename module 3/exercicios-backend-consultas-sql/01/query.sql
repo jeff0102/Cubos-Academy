d2ff41df16edaadbafe7dbbf92fc9c40fc1a085f
jeff0102 <jefferson.jvega@gmail.com>
2023-08-13 12:04:06 -0300

    adapted to seconds

--- module 3/exercicios-backend-consultas-sql/01/query.sql
+++ module 3/exercicios-backend-consultas-sql/01/query.sql
@@ -1,38 +1,14 @@
 -- 1
-SELECT compositor, id FROM musicas;
-
--- 2
-SELECT composicao, tempo FROM musicas WHERE tempo > 4;
+SELECT * FROM musicas;
 
 -- 3
 SELECT compositor, composicao FROM musicas WHERE id BETWEEN 47 AND 123;
-
--- 4
-SELECT * FROM musicas WHERE compositor IS NOT NULL AND tempo < 5 AND compositor <> 'Bach';
 
 -- 5
 SELECT composicao, tempo FROM musicas WHERE compositor IN ('Mozart', 'Bach');
 
 -- 6
 SELECT * FROM musicas ORDER BY id DESC;
-
--- 7
-SELECT * FROM musicas ORDER BY tempo;
-
--- 8
-SELECT * FROM musicas ORDER BY tempo LIMIT 5;
-
--- 9
-SELECT * FROM musicas ORDER BY tempo DESC LIMIT 10;
-
--- 10
-SELECT * FROM musicas WHERE id NOT IN (SELECT id FROM musicas ORDER BY tempo LIMIT 5) ORDER BY tempo LIMIT 10;
-
--- 11
-SELECT * FROM musicas ORDER BY id LIMIT 30 OFFSET 30;
-
--- 12
-SELECT * FROM musicas ORDER BY id LIMIT 72 OFFSET 60;
 
 -- 13
 SELECT DISTINCT compositor FROM musicas WHERE compositor IS NOT NULL;
@@ -41,14 +17,13 @@ SELECT DISTINCT compositor FROM musicas WHERE compositor IS NOT NULL;
 SELECT DISTINCT compositor, composicao FROM musicas;
 
 -- 15
-SELECT * FROM musicas WHERE UPPER(compositor) LIKE 'BRA%';
+SELECT * FROM musicas WHERE compositor LIKE 'Bra%';
 
 -- 16
 SELECT * FROM musicas WHERE ritmo LIKE '% troppo';
 
 -- 17
-SELECT * FROM musicas WHERE UPPER(composicao) LIKE '%QUARTET%';
+SELECT * FROM musicas WHERE composicao ILIKE '%quartet%';
 
 -- 18
-SELECT * FROM musicas WHERE UPPER(composicao) NOT LIKE '%QUINTET%';
-
+SELECT * FROM musicas WHERE composicao NOT ILIKE '%quintet%';
